4cf3f65f0517badd56782c6ed6beb9939379e785
Bryce Watson - MAC <brycedwatson@gmail.com>
2024-04-18 09:42:43 -0700

    Demo very large option (1000)

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
     { value: "3", label: "Option 3", label2: "Bread" },
     { value: "4", label: "Option 4", label2: "Banna" },
   ]
+
+  const VeryLargeOption = Array(1000).fill(1).map((_, i) => {return { value: i, label: `Option ${i}`}});
  
   return (
     <div className="App">
@@ -38,6 +40,17 @@ function App() {
         getOptionLabel={(option) => option.label2}
         />'
         
+      <h3>Hive Selector [Very Large Options]</h3>
+      <p>Here is a very large option list. It contains 1000 options. See how the selector handels overflow.</p>
+      <div style={{width:400}}>
+        <HiveSelector
+          multiple={true} 
+          placeholder="Please make a selection..."
+          options={VeryLargeOption}
+          getOptionLabel={(option) => option.label}
+          />
+      </div>
+
         <h3>Hive Controled Element</h3>
         <p>Here is a controlled element. The value is controled by the parent component.</p>
         <div style={{display:'flex', gap:'10px'}}>
@@ -54,7 +67,6 @@ function App() {
           <div>
             Value: <pre>{JSON.stringify(selectedValue, null, 2)}</pre>
           </div>
-
         </div>
     </div>
   )
